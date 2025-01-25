4856059b9fedd69c4667c40e53f71285b345b21c
Tan Li Lun <116934131+tanlilun@users.noreply.github.com>
2025-01-25 22:47:42 +0800

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,23 +34,23 @@ const sitemap = [
 const socials = [
     {
         label: 'GitHub',
-        href: 'https://www.github.com/codewithsadee-org'
+        href: 'https://github.com/tanlilun'
     },
     {
         label: 'LinkedIn',
-        href: 'https://www.linkedin.com/in/codewithsadee'
+        href: 'https://www.linkedin.com/in/tan-li-lun-1260a8209/'
     },
     {
         label: 'Twitter X',
-        href: 'https://x.com/codewithsadee_'
+        href: 'https://x.com/TanLiLun77'
     },
     {
         label: 'Instagram',
-        href: 'https://www.instagram.com/codewithsadee'
+        href: 'https://www.instagram.com/tanlilun/'
     },
     {
         label: 'CodePen',
-        href: 'https://codepen.io/codewithsadee'
+        href: 'https://codepen.io/Tan-Li-Lun'
     }
 ];
 
