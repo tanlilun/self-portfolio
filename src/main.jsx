b563a56767ed6d25c1d6560ddf038699b6fcadae
Tan Li Lun <116934131+tanlilun@users.noreply.github.com>
2025-01-25 13:59:05 +0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+/**
+ * @copyright 2025 Tan Li Lun
+ * @license Apache-2.0
+ */
+
+/**
+ * Node Modules
+ */
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+/**
+ * Components
+ */
+import App from './App.jsx'
+
+/**
+ * CSS Link
+ */
+import './index.css'
+import 'lenis/dist/lenis.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+import tailwindScrollbar from 'tailwind-scrollbar';
+
+export default {
+  mode: 'jit',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindScrollbar],
+}
